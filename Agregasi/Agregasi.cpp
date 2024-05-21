10a12cd0349071f0b39bf88ec74e81ade57c7b53
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-05-21 11:35:44 +0700

    main ibu dan anak

--- Agregasi/Agregasi.cpp
+++ Agregasi/Agregasi.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
-#include <string>
+#include <vector>
 using namespace std; 
 
 #include "anak.h"
-#inlucde "ibu.h"
+#include "ibu.h"
 
 int main() {
+	ibu* varIbu = new ibu("dini"); 
+	ibu* varIbu2 = new ibu("novi"); 
+	anak* varAnak1 = new anak("tono"); 
+	anak* varAnak2 = new anak("rini"); 
+	anak* varAnak3 = new anak("Dewi"); 
+
+	varIbu->tambahAnak(varAnak1);
+	varIbu->tambahAnak(varAnak2); 
 
 }
